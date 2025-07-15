38753e32285528926b72450afccc19abfe668e16
Wendell Matheus <wendell.araujo@dcx.ufpb.br>
2025-07-15 11:19:34 -0300

    add: a loader animation to wait response

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,16 +1,24 @@
 function enviarComentario() {
-        const comentario = document.getElementById('comentario').value;
+    const comentario = document.getElementById('comentario').value;
+    const tituloResultado = document.getElementById('titulo-resultado');
+    const resultado = document.getElementById('resultado');
 
-        fetch('http://localhost:8080/api/comentario', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ conteudo: comentario })
-        })
-        .then(response => response.text())
-        .then(data =>{
-            document.getElementById('titulo-resultado').style.display = 'block';
-            document.getElementById('titulo-resultado').textContent = 'Resultado da Análise';
-            document.getElementById('resultado').textContent = data;
-        })
-        .catch(error => console.error('Erro:', error));
-    }
+    // Mostra mensagem de carregamento
+    tituloResultado.style.display = 'block';
+    tituloResultado.textContent = 'Analisando...';
+    resultado.innerHTML = '<div class="loader"></div>';
+
+
+    fetch('http://localhost:8080/api/comentario', {
+       method: 'POST',
+       headers: { 'Content-Type': 'application/json' },
+       body: JSON.stringify({ conteudo: comentario })
+    })
+    .then(response => response.text())
+    .then(data =>{
+        document.getElementById('titulo-resultado').style.display = 'block';
+        document.getElementById('titulo-resultado').textContent = 'Resultado da Análise';
+        document.getElementById('resultado').textContent = data;
+    })
+    .catch(error => console.error('Erro:', error));
+}
